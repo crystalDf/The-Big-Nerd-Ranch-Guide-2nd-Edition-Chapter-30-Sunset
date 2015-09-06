ccf7121d5210625cf527259ffc2ae3cac38f8068
crystalDf <chendong333@gmail.com>
2015-09-06 22:32:57 +0800

    Chapter 30 Sunset

---
+++ app/src/main/java/com/star/sunset/SunsetFragment.java
@@ -0,0 +1,25 @@
+package com.star.sunset;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sunset, container, false);
+    }
+
+
+}
